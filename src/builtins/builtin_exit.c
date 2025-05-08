903c023538b66feb788f18b65cf4abc03d836bd0
elouannh <elouannhosta27@gmail.com>
2025-05-08 15:30:56 +0200

    fixed tty and not tty input/output management

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,30 +6,30 @@
 /*   By: ehosta <ehosta@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:00:19 by ehosta            #+#    #+#             */
-/*   Updated: 2025/05/07 16:07:43 by ehosta           ###   ########.fr       */
+/*   Updated: 2025/05/08 15:02:59 by ehosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void		_free_before_exit(t_excmd *c);
-static t_bool	_check_errors(t_excmd *c);
+static t_bool	_check_errors(t_excmd *c, unsigned char *status);
 static t_bool	_is_numeric_only(char *nptr);
 
 t_exit	builtin_exit(t_excmd *c)
 {
-	unsigned char		status;
-	char				*arg;
-	t_bool				free_at_end;
+	unsigned char	status;
+	t_bool			free_at_end;
 
-	(void)arg;
+	if (isatty(STDOUT_FILENO))
+		ft_printf("exit\n");
 	status = (unsigned char)c->vars->minishell->last_status;
 	if (c->argc == 1)
 	{
 		_free_before_exit(c);
 		exit(status);
 	}
-	free_at_end = _check_errors(c);
+	free_at_end = _check_errors(c, &status);
 	if (free_at_end)
 	{
 		_free_before_exit(c);
@@ -47,13 +47,29 @@ static void	_free_before_exit(t_excmd *c)
 	free_env(&c->vars->minishell->env);
 	free(c->vars);
 	free_cmds(cmds);
-	(void)_is_numeric_only;
 }
 
-static t_bool	_check_errors(t_excmd *c)
+static t_bool	_check_errors(t_excmd *c, unsigned char *status)
 {
-	(void)c;
-	return (true);
+	size_t	i;
+
+	i = 0;
+	while (c->argv[++i])
+	{
+		if (_is_numeric_only(c->argv[i]) == false)
+		{
+			puterr(ft_sprintf(": exit: %s: numeric argument required\n", c->argv[i]), false);
+			*status = EXIT_PARSING;
+			return (true);
+		}
+	}
+	if (c->argc == 2)
+	{
+		*status = (unsigned char)ft_atoi(c->argv[1]);
+		return (true);
+	}
+	puterr(ft_sprintf(": exit: too many arguments\n"), false);
+	return (false);
 }
 
 static t_bool	_is_numeric_only(char *nptr)
